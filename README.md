a56c9d174a6ee16514e852234a72b2e51cac56dd
Christopher Lozzi <clozzi12@gmail.com>
2023-09-19 16:14:12 -0400

    search, fetch, display meals added

---
+++ README.md
@@ -0,0 +1,50 @@
+# Phase 1 Project: JS, CSS, HTML
+
+## Project Title
+
+### Built Using
+
+```bash
+Javascript
+```
+
+```bash
+HTML
+```
+
+```bash
+CSS
+```
+
+---
+
+### Overall Functionality
+
+- bulleted uses
+
+---
+
+### Usage
+
+Brief description 
+
+---
+
+### Contact
+
+Email: clozzi12@gmail.com
+
+Git: https://github.com/clozzi
+
+LinkedIn: https://www.linkedin.com/in/christopher-lozzi-823777260/
+
+---
+
+### Acknowledgments
+
+Here are a few of the resources that I found useful during the creation of this project, give them a look!
+
+- [Markdown Guide](https://www.markdownguide.org/basic-syntax/)
+- [w3Schools Tutorials](https://www.w3schools.com/)
+- [MDN Web Docs](https://developer.mozilla.org/en-US/docs/Web)
+- [Free Photo Usage](https://pixabay.com/)
---
+++ script.js
@@ -0,0 +1,32 @@
+document.getElementById('recipe-search').addEventListener('submit', (e) => {
+    let mainIngredient = document.getElementById('main-ingredient').value
+    fetch (`https://www.themealdb.com/api/json/v1/1/filter.php?i=${mainIngredient}`)
+        .then((res) => res.json())
+        .then(mealsObj => mealsObj['meals'].forEach(meal => {
+            displayRecipes(meal)
+        }));
+    e.preventDefault()
+})
+
+function displayRecipes (mealsArr) {
+    const recipeUL = document.getElementById('recipe-results')
+    let mealDiv = document.createElement('div')
+    let mealName = document.createElement('h3')
+    mealName.textContent = mealsArr["strMeal"]
+    mealDiv.append(mealName)
+    let mealImg = document.createElement('img')
+    mealImg.src = mealsArr["strMealThumb"]
+    let mealID = mealsArr['idMeal']
+    mealImg.width = 150
+    mealImg.height = 150
+    mealDiv.append(mealImg) 
+    mealDiv.id = mealID
+    let lineBreak = document.createElement('br')
+    mealDiv.append(lineBreak)
+    let recipeLink = document.createElement('a')
+    recipeLink.href = (`\nhttps://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    recipeLink.textContent = "Find instructions here!"
+    recipeLink.target = '_blank'
+    mealDiv.append(recipeLink)   
+    recipeUL.append(mealDiv)
+}
